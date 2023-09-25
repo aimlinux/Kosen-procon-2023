0380fe4f1b7213001d50533eef1fc7614edbae8c
Kohara <1k8aioon@gmail.com>
2023-09-25 19:13:55 +0900

    c

--- API/test_3.py
+++ API/test_3.py
@@ -3,14 +3,22 @@
 import requests
 
 
-server_url = 'http://localhost:3000' # サーバーのURL
+server_url = "https://procon34system.kosen.work"# サーバーのURL
 token_file = "C:/Users/kxiyt/Desktop/token.txt"
 with open(token_file, encoding="UTF-8") as f:
     f_text = f.read()
 token_text = f_text
 
+# SSL証明書の検証を無効にする
+requests.packages.urllib3.disable_warnings()
+
 try:
+    proxies = {
+        "http":None,
+        "https":None
+    }
     # サーバーから試合状態を取得
+    #response = requests.get(f"{server_url}/state?token={token_text}", verify=False, proxies=proxies)
     response = requests.get(f"{server_url}/state?token={token_text}")
 
     if response.status_code == 200 or response.status_code == 201 or response.status_code == 404:
